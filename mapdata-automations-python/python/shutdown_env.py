d283150535f18b599e30303ad7234960bb622a63
jdassen <joao.felipe.dassencao.faria@ses.com>
2025-05-05 12:02:04 +0200

    removing copy app, correcting shutdown_env script

--- mapdata-automations-python/python/shutdown_env.py
+++ mapdata-automations-python/python/shutdown_env.py
@@ -1,24 +1,22 @@
 import subprocess
 
-def run_script(script_path, args=None):
-    try:
-        command = [script_path] if args is None else [script_path] + args
-        result = subprocess.run(command, check=True, capture_output=True, text=True)
-        print(f"Output: {result.stdout}")
-        return True
-    except subprocess.CalledProcessError as e:
-        print(f"Error running script: {e}")
-        print(f"Error output: {e.stderr}")
-        return False
-
-# Example usage
-scripts_to_run = [
-    "stop_docker_services.py"
+# Lista de diretórios contendo os arquivos docker-compose.yml
+compose_dirs = [
+    "../../mapdata-db/docker/",
+    "../../mapdata-api/docker/",
+    "../../mapdata-app/docker/",
+    #"../../mapdata-infra/git/docker/",
+    #"../../mapdata-infra/jira/docker/",
+    #"../../mapdata-infra/oauth/docker/",
+    #"../../mapdata-infra/sonarqube/docker/"
 ]
 
-for script in scripts_to_run:
-    success = run_script(script)
-    if success:
-        print(f"Successfully ran {script}")
-    else:
-        print(f"Failed to run {script}")
+# Comando base para rodar o Docker Compose
+command = ["docker-compose", "stop"]
+
+# Loop para iniciar os serviços de cada projeto
+for directory in compose_dirs:
+    print(f"Stoping Docker Compose in {directory}...")
+    subprocess.run(command, cwd=directory, check=True)
+
+print("All Docker Compose services Stoping successfully!")
