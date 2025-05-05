752aa7b81e82eae09ceb48362f98f38a8393ab46
jdassen <joao.felipe.dassencao.faria@ses.com>
2025-05-05 11:32:48 +0200

    correcting inner repository issue

--- mapdata-app/src/app/app-routing.module.ts
+++ mapdata-app/src/app/app-routing.module.ts
@@ -1,57 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DataListComponent } from './components/data-list/data-list.component';
 import { DataFormComponent } from './components/data-form/data-form.component';
 import { MetadataListComponent } from './components/metadata-list/metadata-list.component';
 import { MetadataFormComponent } from './components/metadata-form/metadata-form.component';
 
-declare const Keycloak: any; // Declaração para usar o Keycloak diretamente
-
-class KeycloakGuard implements CanActivate {
-  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    const keycloak = new Keycloak({
-      url: 'http://localhost:6080/', // URL do Keycloak
-      realm: 'mapdata-realm', // Nome do Realm
-      clientId: 'mapdata-client', // ID do Cliente
-    });
-
-    // Inicializa o Keycloak
-    const authenticated = await keycloak.init({
-      onLoad: 'check-sso', // Verifica se o usuário está autenticado
-      checkLoginIframe: false,
-    });
-
-    if (!authenticated) {
-      keycloak.login(); // Redireciona para o login do Keycloak
-      return false;
-    }
-
-    return true; // Permite o acesso à rota
-  }
-}
-
 const routes: Routes = [
   {
     path: 'data-list',
-    component: DataListComponent,
-    canActivate: [KeycloakGuard], // Protege a rota com o guard personalizado
+    component: DataListComponent
   },
   {
     path: 'data-form',
-    component: DataFormComponent,
-    canActivate: [KeycloakGuard], // Protege a rota com o guard personalizado
+    component: DataFormComponent
   },
   {
     path: 'metadata-list',
-    component: MetadataListComponent,
-    canActivate: [KeycloakGuard], // Protege a rota com o guard personalizado
+    component: MetadataListComponent
   },
   {
     path: 'metadata-form',
-    component: MetadataFormComponent,
-    canActivate: [KeycloakGuard], // Protege a rota com o guard personalizado
+    component: MetadataFormComponent
   },
-  { path: '**', redirectTo: 'data-list' }, // Redireciona rotas inválidas para 'data-list'
+  { path: '**', redirectTo: 'data-list' }, // Redirect to data-list for any unknown routes
 ];
 
 @NgModule({
--- mapdata-app/src/app/app.module.ts
+++ mapdata-app/src/app/app.module.ts
@@ -3,17 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { KeycloakService } from './services/keycloak.service';
 import { DataListComponent } from './components/data-list/data-list.component';
 import { DataFormComponent } from './components/data-form/data-form.component';
 import { MetadataListComponent } from './components/metadata-list/metadata-list.component';
 import { MetadataFormComponent } from './components/metadata-form/metadata-form.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-function initializeKeycloak(keycloak: KeycloakService) {
-  return () => keycloak.init();
-}
 
 @NgModule({
   declarations: [
@@ -29,14 +24,7 @@ function initializeKeycloak(keycloak: KeycloakService) {
     FormsModule,
     HttpClientModule
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService],
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
